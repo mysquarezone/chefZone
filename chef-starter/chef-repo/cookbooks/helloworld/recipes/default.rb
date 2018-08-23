70c0ba916434d04d12639910da718dc72512915b
rskr87 <reddy.shivareddem@gmail.com>
2018-08-23 11:03:55 +0530

    added resource nginx to recipe

--- chef-starter/chef-repo/cookbooks/helloworld/recipes/default.rb
+++ chef-starter/chef-repo/cookbooks/helloworld/recipes/default.rb
@@ -2,6 +2,15 @@
 # Cookbook:: helloworld
 # Recipe:: default
 #
-# test edit
 
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# write a resource for installing git
+
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action :start
+end
