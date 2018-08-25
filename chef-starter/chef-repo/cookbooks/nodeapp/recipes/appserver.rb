5712853411a423ac79b73ca6621440e88ec0bbb4
rskr87 <reddy.shivareddem@gmail.com>
2018-08-25 16:22:18 +0530

    command

--- chef-starter/chef-repo/cookbooks/nodeapp/recipes/appserver.rb
+++ chef-starter/chef-repo/cookbooks/nodeapp/recipes/appserver.rb
@@ -20,12 +20,12 @@ end
 
 #download tar file from the location "https://nodejs.org/dist/v8.11.4/node-v8.11.4-linux-x64.tar.xz"
 
-remote_file 'node['appserver']['nodejs_temp_path']/node-v8.11.4-linux-x64.tar.xz' do
-  source 'https://nodejs.org/dist/v8.11.4/node-v8.11.4-linux-x64.tar.xz'
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
+remote_file "'node['appserver']['nodejs_temp_path']'/node-v8.11.4-linux-x64.tar.xz" do
+    source 'https://nodejs.org/dist/v8.11.4/node-v8.11.4-linux-x64.tar.xz'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
 end
 
 directory node['appserver']['node_executables'] do
@@ -36,7 +36,7 @@ directory node['appserver']['node_executables'] do
 end
 
 execute 'untarnodeexecutables' do
-    command 'tar xvf node-v*.tar.?z --strip-components=1 -C node['appserver']['node_executables']'
+    command "tar xvf node-v*.tar.?z --strip-components=1 -C 'node['appserver']['node_executables']'"
 end
 
 directory node['appserver']['node_executables']/etc do
@@ -47,11 +47,11 @@ directory node['appserver']['node_executables']/etc do
 end
 
 execute 'echo command execute' do
-    command 'echo 'prefix=/usr/local' > node/etc/npmrc'
+    command echo 'prefix=/usr/local' > node/etc/npmrc
 end
 
 remote_directory '/opt/' do
-  source 'node['appserver']['node_executables']'
+  source node['appserver']['node_executables']
   owner 'root'
   group 'root'
   mode '0755'
@@ -74,13 +74,8 @@ link '/usr/local/bin/npm' do
     link_type :symbolic
 end
 
-file 'node['appserver']['node_executables']/hello.js' do
-    content 'var http = require('http');
-    http.createServer(function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Hello World\n');
-    }).listen(8080, '172.31.4.196');
-    console.log('Server running at http://172.31.4.196:8080/');'
+file node['appserver']['node_executables']/hello.js do
+    content "var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('Hello World\n'); }).listen(8080, '172.31.4.196'); console.log('Server running at http://172.31.4.196:8080/');"
     mode '0755'
 end
 
@@ -93,5 +88,5 @@ execute 'Install npm' do
 end
 
 execute 'start hello.js' do
-    command 'pm2 start node['appserver']['node_executables']/hello.js'
+    command pm2 start node['appserver']['node_executables']/hello.js
 end
